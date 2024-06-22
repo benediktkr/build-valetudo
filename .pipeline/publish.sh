2226f75ee21df6280fca4ece0eba827b5b35b93f
Ben K <ben@sudo.is>
2024-06-22 21:34:51 +0200

    gitea api release fixes

--- .pipeline/publish.sh
+++ .pipeline/publish.sh
@@ -15,11 +15,11 @@ gitea_upload_file() {
 }
 
 gitea_release_create() {
-    curl -s -X 'POST' \
-  'https://${GITEA_SECRET}@${GITEA_URL}/api/v1/repos/${GITEA_USER}/build-valetudo/releases' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
+    curl -s -X POST \
+      https://${GITEA_SECRET}@${GITEA_URL}/api/v1/repos/${GITEA_USER}/build-valetudo/releases \
+      -H 'accept: application/json' \
+      -H 'Content-Type: application/json' \
+      -d '{
   "body": "Valetudo release: ${VERSION}",
   "draft": false,
   "name": "Valetudo ${VERSION}",
@@ -29,14 +29,24 @@ gitea_release_create() {
 }' | jq .
 }
 
+gitea_release_file() {
+    curl -X POST \
+      https://${GITEA_SECRET}@${GITEA_URL}/api/v1/repos/${GITEA_USER}/build-valetudo/releases/$1/assets?name=$2 \
+  -H 'accept: application/json' \
+  -H 'Content-Type: multipart/form-data' \
+  -F "attachment=@$2;type=application/x-sh" | jq .
+}
 
+gitea_release_create || true
 
 for arch in "aarch64" "armv7"; do
-    gitea_upload_file "valetudo_${VALETUDO_VERSION}_${arch}.tar.gz"
+    tarball="valetudo_${VALETUDO_VERSION}_${arch}.tar.gz"
+    gitea_upload_file $tarball 
+    gitea_release_file $tarball
 done
 
 for item in "openapi.json" "changelog.md" "changelog_nightly.md" "valetudo_release_manifest.json"; do
     gitea_upload_file ${item}
 done
 
-gitea_release_create || true
+
